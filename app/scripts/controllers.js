c1659fff7a0363e42af754eb8e5a615729f4bbff
Toni Ristola <tristola@gmail.com>
2014-10-30 08:06:41 +0200

    parsing hesari too

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -7,11 +7,17 @@ angular.module('Sarjakuvat.controllers', [])
 	$scope.iltalehtisrc='http://static.iltalehti.fi/sarjakuvat/Fingerpori_'+date+'.gif';
 })
 
-.controller('FriendsCtrl', function($http,$scope, Friends) {
+.controller('FriendsCtrl', function($http,$scope,$log, Friends) {
+  $scope.hshtml='waiting';
   $http.get('http://m.hs.fi/fingerpori/').
   success(function(data, status, headers, config) {
+  	var start=data.indexOf('<img alt="" src="http://hs13.snstatic.fi/webkuva/sarjis');
+  	var end=data.indexOf('>',start);
+  	$scope.hshtml = data.substring(start,end+1);
   }).
   error(function(data, status, headers, config) {
+  	$log.log('getting failed');
+  	$scope.hshtml='getting image failed';
     // called asynchronously if an error occurs
     // or server returns response with an error status.
   });
